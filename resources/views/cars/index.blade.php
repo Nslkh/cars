b6f59539a7da656d2254790c178b95639e850785
Nslkh <olluzran@gmail.com>
2021-04-01 01:17:32 +0500

    Create page user-interface Done

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="m-auto  w-4/5 py-24">
     <div class="text-center">
-      <h1 class="text-5xl uppercase-bold">
+      <h1 class="text-5xl uppercase bold">
         Cars
       </h1>
     </div>
