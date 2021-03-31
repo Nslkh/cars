6d6049011cad20aee36bc1070088ddc92bf15754
Nslkh <olluzran@gmail.com>
2021-03-31 23:27:13 +0500

    core ui project ready

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="m-auto  w-4/5 py-24">
+    <div class="text-center">
+      <h1 class="text-5xl uppercase-bold">
+        Cars
+      </h1>
+    </div>
+  
+
+    <div class="w-5/6 py-10">
+      <div class="m-auto">
+        <span class="uppercase text-blue-500 font-bold text-xs italic"> 
+          Founded:2021
+        </span>
+        <h2 class="text-gray-700 text-5xl">
+          Toyota
+        </h2>
+        <p class="text-lg text-gray-700 py-6">
+          The Toyota Motor Corporation is a Japanese multinational automotive manufacturer headquartered in Toyota, Aichi, Japan. It was founded by Kiichiro Toyoda and incorporated on August 28, 1937
+        </p>
+        <hr class="mt-4 mb-8">
+      </div>
+    </div>
+  </div>
+@endsection
