1a3aa22647f87a9b1dd8f8e9b2dc0a61277389d8
Nslkh <olluzran@gmail.com>
2021-04-03 14:19:42 +0500

    engine model and table created Eloquent hasMany, hasOne relationship

--- app/Models/Engine.php
+++ app/Models/Engine.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Headquarter extends Model
+class Engine extends Model
 {
     use HasFactory;
-
-    public function car()
-    {
-        return $this->belongsTo(Car::class);
-    }
 }
